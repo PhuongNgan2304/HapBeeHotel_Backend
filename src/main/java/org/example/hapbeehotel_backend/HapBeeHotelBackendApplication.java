c8988c15d533a6a6b5b634a196cc1f6a2dbff39e
Phuong Ngan <phuongngan.nguyen2304@gmail.com>
2024-06-01 00:03:45 +0700

    first commit

---
+++ src/main/java/org/example/hapbeehotel_backend/HapBeeHotelBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.hapbeehotel_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HapBeeHotelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HapBeeHotelBackendApplication.class, args);
+	}
+
+}
